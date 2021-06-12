11bc91d308c909704396ea0cda97351d07d0289f
iamjinlei <i.am.jin.lei@gmail.com>
2021-06-12 10:19:28 +0800

    bolling bands

---
+++ bbands.go
@@ -0,0 +1,38 @@
+package tart
+
+type BBands struct {
+	ma        *Ma
+	stdDev    *StdDev
+	upNStdDev float64
+	dnNStdDev float64
+}
+
+func NewBBands(t MaType, n int64, upNStdDev, dnNStdDev float64) *BBands {
+	return &BBands{
+		ma:        NewMa(t, n),
+		stdDev:    NewStdDev(n),
+		upNStdDev: upNStdDev,
+		dnNStdDev: dnNStdDev,
+	}
+}
+
+// upper, middle, lower
+func (b *BBands) Update(v float64) (float64, float64, float64) {
+	m := b.ma.Update(v)
+	stddev := b.stdDev.Update(v)
+
+	return m + b.upNStdDev*stddev, m, m - b.upNStdDev*stddev
+}
+
+func BBandsArr(in []float64, t MaType, n int64, upNStdDev, dnNStdDev float64) ([]float64, []float64, []float64) {
+	m := make([]float64, len(in))
+	u := make([]float64, len(in))
+	l := make([]float64, len(in))
+
+	b := NewBBands(t, n, upNStdDev, dnNStdDev)
+	for i, v := range in {
+		u[i], m[i], l[i] = b.Update(v)
+	}
+
+	return u, m, l
+}
---
+++ bbands_test.go
@@ -0,0 +1,12 @@
+package tart
+
+import (
+	"testing"
+)
+
+func TestBBands(t *testing.T) {
+	upper, mid, lower := BBandsArr(testClose, SMA, 5, 2.0, 2.0)
+	compare(t, "result, mid, lower = talib.BBANDS(testClose, 5, 2.0, 2.0)", upper)
+	compare(t, "upper, result, lower = talib.BBANDS(testClose, 5, 2.0, 2.0)", mid)
+	compare(t, "upper, mid, result = talib.BBANDS(testClose, 5, 2.0, 2.0)", lower)
+}
