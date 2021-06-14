44bd91f54fcd7a9382ec3cd642c4abcf51355507
iamjinlei <i.am.jin.lei@gmail.com>
2021-06-14 15:37:53 +0800

    wma to use cbuf

--- wma.go
+++ wma.go
@@ -3,8 +3,7 @@ package tart
 type Wma struct {
 	n    int64
 	d    float64
-	sz   int64
-	hist []float64
+	hist *cBuf
 	sum  float64
 	wsum float64
 }
@@ -13,8 +12,7 @@ func NewWma(n int64) *Wma {
 	return &Wma{
 		n:    n,
 		d:    float64(n*(n+1)) / 2,
-		sz:   0,
-		hist: make([]float64, n),
+		hist: newCBuf(n),
 		sum:  0,
 		wsum: 0,
 	}
@@ -25,14 +23,12 @@ func (w *Wma) Update(v float64) float64 {
 		return v
 	}
 
-	idx := w.sz % w.n
-	w.sum += v - w.hist[idx]
-	w.hist[idx] = v
+	old := w.hist.append(v)
+	w.sum += v - old
 
-	w.sz++
-
-	if w.sz < w.n {
-		w.wsum += v * float64(w.sz)
+	sz := w.hist.size()
+	if sz < w.n {
+		w.wsum += v * float64(sz)
 		return 0
 	} else {
 		w.wsum += v * float64(w.n)
